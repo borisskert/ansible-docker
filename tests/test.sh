f72ad5142240c46da2d69ad7e2775cc70f8d1f91
Boris Skert <boris.skert@gmail.com>
2020-04-21 18:53:25 +0200

    Support storage-opts

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@ set -e
 vagrant up --provision
 
 echo "Waiting for answer on port 22..."
-while ! timeout 300 nc -z 192.168.33.10 22; do
+while ! timeout 300 nc -z 192.168.33.68 22; do
   sleep 0.2
 done
 
@@ -15,9 +15,9 @@ ansible-playbook -i inventory.ini test.yml \
   && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1)
 
-curl -s http://192.168.33.10:8080 \
+curl -s http://192.168.33.68:8080 \
  | grep -q '</html>' \
   && (echo 'curl test: pass' && exit 0) \
   || (echo 'curl test: fail' && exit 1)
 
-vagrant destroy -f
+#vagrant destroy -f
